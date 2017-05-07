8a15f819671f4e791bceae29d555e19e3e56ba0c
jairogp <jairogp@users.noreply.github.com>
2017-05-07 05:38:38 -0300

    Add files via upload

---
+++ Exemplo_Controle_de_1_Servo_Motor/Exemplo_Controle_de_1_Servo_Motor.ino
@@ -0,0 +1,33 @@
+// Controle de 1 servo
+
+#include <Servo.h>
+
+Servo servo1;
+
+void setup()
+{
+servo1.attach(3);
+}
+
+void loop()
+{
+  servo1.write(10);
+  delay (1000);
+    servo1.write(180);
+  delay (1000);
+    servo1.write(30);
+  delay (1000);
+    servo1.write(160);
+  delay (1000);
+    servo1.write(50);
+  delay (1000);
+    servo1.write(140);
+  delay (1000);
+    servo1.write(70);
+  delay (1000);
+    servo1.write(120);
+  delay (1000);
+    servo1.write(90);
+  delay (1000);
+}
+
